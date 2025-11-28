9213c67e9ecdd06c13a237e6c8dbd498087ae26f
MrPK_ <97881288+MrPk9727@users.noreply.github.com>
2025-11-28 21:40:00 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// All configuration options
+const CONFIG = {
+  title: "Coming soon...",
+  description:
+    "We're working on something amazing. Join our community on Discord to stay up to date..",
+  discordLink: "https://discord.gg/UeuFp4wqEs",
+  buttonText: "Join to discord",
+  logo: "logo.png", // URL of the logo image
+  //background: "linear-gradient(135deg, #667eea 0%, #764ba2 100%)", // Background (color, gradient o URL png/jpg)
+  background: "url('./background.jpeg')",
+  loaderText: "Please wait...", // Text under the loader
+  favicon: "favicon.png", // URL del favicon
+};
